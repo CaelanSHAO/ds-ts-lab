378df8829f450e49fde630c1556121c84a7c81d1
JIAYI SHAO <sjy08015342@163.com>
2025-02-06 12:43:53 +0000

    Typed function signature.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,25 +1,5 @@
 // (Optional) Change the names below to your friends.
-
-interface ColleagueHistory {
-    current: Colleague[];
-    former: Colleague[];
-  }
-  
-interface Colleague {
-    name: string;
-    department: string;
-    contact: {
-      email: string;
-      extension: number;
-    };
-  }
-
-  
-interface Friend {
-    name: string;
-    phone: string;
-    age: number;
-  }
+import {Friend, Colleague, ColleagueHistory } from './myTypes'
   
 
 const friend1 : Friend = {
@@ -34,8 +14,8 @@ const friend1 : Friend = {
     age: 31,
   };
   
-  const friends : Friend[]= [friend1, friend2];
-  console.log(friends[1]);
+export  const friends : Friend[]= [friend1, friend2];
+  //console.log(friends[1]);
   
   //   -------------------
   
@@ -65,10 +45,12 @@ const friend1 : Friend = {
       extension: 125,
     },
   };
-  const colleagues : ColleagueHistory= {
+  export const colleagues : ColleagueHistory= {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
   
-  console.log(colleagues.current[0]);
+ // console.log(colleagues.current[0]);
+
+
   
